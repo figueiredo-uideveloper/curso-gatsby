a39115b1ec46417b37b08026486d4fa36959e3db
figueiredo-uideveloper <uideveloper@figueiredo.dev>
2020-01-23 18:34:56 -0200

    Config styled-components and config avatar

---
+++ src/components/Avatar/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+import Img from "gatsby-image"
+
+export const AvatarWrapper = styled(Img)`
+    border-radius: 100px;
+    height: 3.75rem;
+    width: 3.75rem;
+    margin: auto;
+`
